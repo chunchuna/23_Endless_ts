21f2565c6b3dc1fda023c531a80045248de6a5f3
纯纯 <xiangjunjie@libii.com>
2023-12-12 14:14:50 +0800

    frist comiit

---
+++ scripts/importsForEvents.js
@@ -0,0 +1,4 @@
+"use strict";
+// Put imports here that you wish to use for script blocks in event sheets, e.g.:
+// import * as myModule from "./mymodule.js";
+// Then you can use 'myModule' in script blocks in event sheets.
