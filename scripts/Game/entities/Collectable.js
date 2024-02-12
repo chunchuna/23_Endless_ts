a1662d4cccc8e9e75e1ed6d108ea53ad607bc90f
chunchun <578806739@qq.com>
2024-02-12 13:33:02 +0800

    修改collectableclass

--- scripts/Game/entities/Collectable.js
+++ scripts/Game/entities/Collectable.js
@@ -13,23 +13,50 @@ export class Collectable extends ConstructSystem {
         await (EventHnadlerInstance?.addEventListener)("OnMouseClickOneceCollectableGroup", (e) => {
             Collectable.OnMouseClickColGroup(runtime, e);
         });
+        await runtime.objects.CollectableGroup.addEventListener("instancedestroy", (e) => {
+            this.OnCollectDestory(runtime, e);
+        });
     }
     static OnMouseClickColGroup(runtime, e) {
-        const getCollectableStuff = e.detail[0];
-        if (getCollectableStuff.instVars["Distance"] > runtime.globalVars.CollectableEffectiveRange)
-            return;
-        if (getCollectableStuff.instVars["Heath"] > 0) {
-            getCollectableStuff.instVars["Heath"] -= 1;
-            Collectable.CollectableStuffShakeOnece(runtime, getCollectableStuff);
-        }
-        else if (getCollectableStuff.instVars["Heath"] <= 0) {
-            getCollectableStuff.destroy();
-        }
+        this.HitCollectOnece(runtime, e.detail[0]);
     }
     static OnMouseOverColGroup(runtime) {
         document.body.style.cursor = 'pointer';
     }
+    static OnCollectDestory(runtime, e) {
+        this.CreatGiftForCollecting(runtime, e);
+    }
     /** Function **/
+    static CollectHeath(runtime, Collect, newHeath) {
+        if (newHeath !== undefined) {
+            // Setter
+            Collect.instVars.Heath = newHeath;
+        }
+        else {
+            // Getter
+            return Collect.instVars.Heath;
+        }
+    }
+    static GetCollectDistance(runtime, Collect) {
+        return Collect.instVars.Distance;
+    }
+    static HitCollectOnece(runtime, Collect) {
+        var Distance = this.GetCollectDistance(runtime, Collect);
+        var Heath = this.CollectHeath(runtime, Collect);
+        if (Distance > runtime.globalVars.CollectableEffectiveRange) {
+            return;
+        }
+        if (Heath > 0) {
+            this.CollectableStuffShakeOnece(runtime, Collect);
+            var newHeath = Heath - runtime.globalVars.CollectablePlayerDmg;
+            this.CollectHeath(runtime, Collect, newHeath);
+        }
+        if (Heath <= 0) {
+            Collect.destroy();
+        }
+    }
+    static CreatGiftForCollecting(runtime, Collect) {
+    }
     static CollectableStuffShakeOnece(runtime, CollectableInstance) {
         const Instance = CollectableInstance;
         if (!Instance)
--- scripts/Game/entities/Collectable.ts
+++ scripts/Game/entities/Collectable.ts
@@ -22,17 +22,14 @@ export class Collectable extends ConstructSystem {
             Collectable.OnMouseClickColGroup(runtime, e);
         });
 
+        await runtime.objects.CollectableGroup.addEventListener("instancedestroy", (e) => {
+            this.OnCollectDestory(runtime, e)
+        })
+
     }
 
     private static OnMouseClickColGroup(runtime: IRuntime, e: any) {
-        const getCollectableStuff: InstanceType.CollectableGroup = e.detail[0];
-        if (getCollectableStuff.instVars["Distance"] > runtime.globalVars.CollectableEffectiveRange) return;
-        if (getCollectableStuff.instVars["Heath"] > 0) {
-            getCollectableStuff.instVars["Heath"] -= 1;
-            Collectable.CollectableStuffShakeOnece(runtime, getCollectableStuff);
-        } else if (getCollectableStuff.instVars["Heath"] <= 0) {
-            getCollectableStuff.destroy();
-        }
+        this.HitCollectOnece(runtime, e.detail[0]);
 
     }
 
@@ -41,8 +38,53 @@ export class Collectable extends ConstructSystem {
 
     }
 
+    private static OnCollectDestory(runtime: IRuntime, e: any) {
+        this.CreatGiftForCollecting(runtime, e);
+
+    }
+
 
     /** Function **/
+
+    private static CollectHeath(runtime: IRuntime, Collect: InstanceType.CollectableGroup, newHeath?: number): number | void {
+        if (newHeath !== undefined) {
+            // Setter
+            Collect.instVars.Heath = newHeath;
+        } else {
+            // Getter
+            return Collect.instVars.Heath;
+        }
+    }
+
+    private static GetCollectDistance(runtime: IRuntime, Collect: InstanceType.CollectableGroup) {
+        return Collect.instVars.Distance;
+    }
+
+    private static HitCollectOnece(runtime: IRuntime, Collect: InstanceType.CollectableGroup) {
+
+        var Distance = this.GetCollectDistance(runtime, Collect)!;
+        var Heath = this.CollectHeath(runtime, Collect)!;
+
+        if (Distance > runtime.globalVars.CollectableEffectiveRange) {
+            return;
+        }
+
+        if (Heath > 0) {
+            this.CollectableStuffShakeOnece(runtime, Collect)
+            var newHeath = Heath - runtime.globalVars.CollectablePlayerDmg;
+            this.CollectHeath(runtime, Collect, newHeath);
+        }
+
+        if (Heath <= 0) {
+            Collect.destroy()
+        }
+
+
+    }
+
+    private static CreatGiftForCollecting(runtime: IRuntime, Collect: InstanceType.CollectableGroup) {
+
+    }
 
     private static CollectableStuffShakeOnece(runtime: IRuntime, CollectableInstance: InstanceType.CollectableGroup) {
         const Instance = CollectableInstance;
